f43cbbe7cad9908b99f4efbfa50e890bdf6cf34d
Kevin Lindsey <kevin@kevlindev.com>
2016-05-30 12:07:35 -0700

    Initial Commit

---
+++ RayTracingWeekend/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RayTracingWeekend
+//
+//  Created by Kevin Lindsey on 5/30/16.
+//  Copyright © 2016 Kevin Lindsey. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
