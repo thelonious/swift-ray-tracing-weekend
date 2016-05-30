4efc299691da068c9130dea78cfcb3299602b8b2
Kevin Lindsey <kevin@kevlindev.com>
2016-05-30 13:55:17 -0700

    Add simple camera and antialiasing support

--- RayTracingWeekend/camera.swift
+++ RayTracingWeekend/camera.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+
+struct camera {
+    let lower_left_corner = vec3(x: -2.0, y: -1.0, z: -1.0)
+    let horizontal = vec3(x: 4.0, y: 0, z: 0)
+    let vertical = vec3(x: 0, y: 2.0, z: 0)
+    let origin = vec3(x: 0, y: 0, z: 0)
+    
+    func get_ray(u: Double, _ v: Double) -> ray {
+        return ray(origin: origin, direction: lower_left_corner + u * horizontal + v * vertical - origin);
+    }
+}
--- RayTracingWeekend/main.swift
+++ RayTracingWeekend/main.swift
@@ -26,6 +26,7 @@ func color(r: ray, world: hitable) -> vec3 {
 
 let nx = 200
 let ny = 100
+let ns = 100
 
 print("P3")
 print(nx, ny)
@@ -37,19 +38,31 @@ let vertical = vec3(x: 0.0, y: 2.0, z: 0.0)
 let origin = vec3(x: 0.0, y: 0.0, z: 0.0)
 
 let world = hitable_list()
-var object = sphere(c: vec3(x: 0, y: 0, z: -1), r: 0.5)
-world.add(object)
-object = sphere(c: vec3(x: 0, y: -100.5, z: -1), r: 100)
-world.add(object)
+world.add(sphere(c: vec3(x: 0, y: 0, z: -1), r: 0.5))
+world.add(sphere(c: vec3(x: 0, y: -100.5, z: -1), r: 100))
+
+let cam = camera()
 
 for j in (ny - 1).stride(through: 0, by: -1) {
     for i in 0..<nx {
-        let u = Double(i) / Double(nx)
-        let v = Double(j) / Double(ny)
-        let r = ray(origin: origin, direction: lower_left_corner + u * horizontal + v * vertical)
+        var col = vec3(x: 0, y: 0, z: 0)
         
-        let p = r.point_at_parameter(2.0)
-        let col = color(r, world: world)
+        for s in 0..<ns {
+            let u = (Double(i) + drand48()) / Double(nx)
+            let v = (Double(j) + drand48()) / Double(ny)
+            let r = cam.get_ray(u, v)
+            let p = r.point_at_parameter(2.0)
+            
+            col = col + color(r, world: world)
+        }
+        
+        col = col / Double(ns)
+        
+//        let u = Double(i) / Double(nx)
+//        let v = Double(j) / Double(ny)
+//        let r = cam.get_ray(u, v)
+//        let p = r.point_at_parameter(2.0)
+//        let col = color(r, world: world)
         
         let ir = UInt8(255 * col.r)
         let ig = UInt8(255 * col.g)
