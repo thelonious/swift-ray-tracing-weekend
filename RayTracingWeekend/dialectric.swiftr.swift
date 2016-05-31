958201b7a75a5cccc185e079be1a2bd43cb91bd0
Kevin Lindsey <kevin@kevlindev.com>
2016-05-30 18:58:03 -0700

    Delete unused file

--- RayTracingWeekend/dialectric.swiftr.swift
+++
@@ -1,9 +0,0 @@
-//
-//  dialectric.swiftr.swift
-//  RayTracingWeekend
-//
-//  Created by Kevin Lindsey on 5/30/16.
-//  Copyright © 2016 Kevin Lindsey. All rights reserved.
-//
-
-import Foundation
