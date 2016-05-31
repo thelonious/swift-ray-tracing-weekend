2223879393070028bbab7e212fdbd302568ac18a
Kevin Lindsey <kevin@kevlindev.com>
2016-05-30 18:16:38 -0700

    Create lambertian and metal materials

--- RayTracingWeekend/lambertian.swift
+++ RayTracingWeekend/lambertian.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+class lambertian: Material {
+    var albedo: vec3
+    
+    init(a: vec3) {
+        albedo = a
+    }
+    
+    func scatter(ray_in: ray, _ rec: hit_record, inout _ attenuation: vec3, inout _ scattered: ray) -> Bool {
+        let target = rec.p + rec.normal + random_in_unit_sphere()
+        
+        scattered = ray(origin: rec.p, direction: target - rec.p)
+        attenuation = albedo
+        
+        return true
+    }
+}
--- RayTracingWeekend/metal.swift
+++ RayTracingWeekend/metal.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+class metal: Material {
+    var albedo: vec3
+    
+    init(a: vec3) {
+        albedo = a
+    }
+    
+    func scatter(ray_in: ray, _ rec: hit_record, inout _ attenuation: vec3, inout _ scattered: ray) -> Bool {
+        let reflected = ray_in.direction.unit_vector().reflect(rec.normal)
+        
+        scattered = ray(origin: rec.p, direction: reflected)
+        attenuation = albedo
+        
+        return scattered.direction.dot(rec.normal) > 0
+    }
+}
