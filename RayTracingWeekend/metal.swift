43b9d5cf158ec3b9d404b92c9e2d1a1b225e54d1
Kevin Lindsey <kevin@kevlindev.com>
2016-05-30 18:58:18 -0700

    Add fuzz parameter to metal material

--- RayTracingWeekend/metal.swift
+++ RayTracingWeekend/metal.swift
@@ -10,15 +10,17 @@ import Foundation
 
 class metal: Material {
     var albedo: vec3
+    var fuzz: Double
     
-    init(a: vec3) {
+    init(a: vec3, f: Double) {
         albedo = a
+        fuzz = f < 1 ? f : 1
     }
     
     func scatter(ray_in: ray, _ rec: hit_record, inout _ attenuation: vec3, inout _ scattered: ray) -> Bool {
         let reflected = ray_in.direction.unit_vector().reflect(rec.normal)
         
-        scattered = ray(origin: rec.p, direction: reflected)
+        scattered = ray(origin: rec.p, direction: reflected + fuzz * random_in_unit_sphere())
         attenuation = albedo
         
         return scattered.direction.dot(rec.normal) > 0
