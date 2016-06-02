4a1d01157c63efb6bdd65dcad44030377c0001f6
Kevin Lindsey <kevin@kevlindev.com>
2016-06-02 13:44:24 -0700

    Allow arbitrary placement of the camera

--- RayTracingWeekend/camera.swift
+++ RayTracingWeekend/camera.swift
@@ -13,25 +13,38 @@ struct Camera {
     let horizontal: Vec3
     let vertical: Vec3
     let origin: Vec3
+    let lensRadius: Double
+    let u: Vec3
+    let v: Vec3
+    let w: Vec3
     
-    init(lookFrom: Vec3, lookAt: Vec3, vup: Vec3, vfov: Double, aspect: Double) {
+    init(lookFrom: Vec3, lookAt: Vec3, vup: Vec3, vfov: Double, aspect: Double, aperture: Double, focus_dist: Double) {
+        lensRadius = aperture * 0.5
+        
         let theta = vfov * M_PI / 180.0
         let half_height = tan(theta * 0.5)
         let half_width = aspect * half_height
         
         origin = lookFrom
         
-        let w = (lookFrom - lookAt).unit_vector()
-        let u = vup.cross(w).unit_vector()
-        let v = w.cross(u)
+        w = (lookFrom - lookAt).unit_vector()
+        u = vup.cross(w).unit_vector()
+        v = w.cross(u)
         
-        lower_left_corner = origin - half_width * u - half_height * v - w
-        horizontal = 2.0 * half_width * u
-        vertical = 2.0 * half_height * v
+        lower_left_corner = origin - (half_width * focus_dist * u) - (half_height * focus_dist * v) - focus_dist * w
+        horizontal = 2.0 * half_width * focus_dist * u
+        vertical = 2.0 * half_height * focus_dist * v
         
     }
     
-    func get_ray(u: Double, _ v: Double) -> Ray {
-        return Ray(origin: origin, direction: lower_left_corner + u * horizontal + v * vertical - origin);
+    func get_ray(s: Double, _ t: Double) -> Ray {
+        let rd = lensRadius * random_in_unit_disk()
+        let offset = u * rd.x + v * rd.y
+//        let offset = Vec3(x: 0, y: 0, z: 0)
+        
+        return Ray(
+            origin: origin + offset,
+            direction: lower_left_corner + (s * horizontal) + (t * vertical) - origin - offset
+        );
     }
 }
